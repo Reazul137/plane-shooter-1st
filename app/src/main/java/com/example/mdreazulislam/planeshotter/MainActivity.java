0b954482e66c7305ccc881e195d370f54318208b
Reazul Islam <reazulislam137@gmail.com>
2018-03-19 19:37:06 +0600

    first commit

---
+++ app/src/main/java/com/example/mdreazulislam/planeshotter/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.mdreazulislam.planeshotter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View v){
+
+        Log.i("ImageButton","Clicked");
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/mdreazulislam/planeshotter/StartGame.java
@@ -0,0 +1,16 @@
+package com.example.mdreazulislam.planeshotter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Md Reazul Islam on 3/18/2018.
+ */
+
+public class StartGame extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
